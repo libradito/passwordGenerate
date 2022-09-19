104321ace4de3f86b1f92edcc59e2b80ab847d1d
libradito <47046453+libradito@users.noreply.github.com>
2022-09-19 14:35:17 -0700

    buttons functionality added

--- index.js
+++ index.js
@@ -91,3 +91,149 @@ const characters = [
   "?",
   "/",
 ];
+const removedNumbers = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+const generatePw = document.getElementById("pw-btn");
+const passwordOne = document.getElementById("pwOne");
+const passwordTwo = document.getElementById("pwTwo");
+const passwordLength = document.getElementById("length");
+const withSymbols = document.getElementById("symbols");
+const withNumbers = document.getElementById("numbers");
+const noSymbols = characters;
+
+const removedSymbols = noSymbols.slice(start, 61);
+
+const noneChecked = characters;
+const noChecked = noneChecked.slice(start, 52);
+
+let pwLength = 0;
+
+function start() {
+  pwLength = passwordLength.value;
+  let randomPw = [];
+  let randomTwo = [];
+  generateRandom(randomPw);
+  generateRandom(randomTwo);
+
+  passwordOne.textContent = randomPw.join("");
+  passwordTwo.textContent = randomTwo.join("");
+
+  console.log(randomPw);
+  console.log(randomTwo);
+}
+
+generatePw.addEventListener("click", start, false);
+
+function generateRandom(toGenerate) {
+  if (withNumbers.checked === true && withSymbols.checked === true) {
+    console.log("BothCheked");
+    for (let i = 0; i < pwLength; i++) {
+      let randomNum = Math.floor(Math.random() * characters.length) + 1;
+      toGenerate.push(characters[randomNum]);
+    }
+  } else if (withNumbers.checked === true) {
+    for (let i = 0; i < pwLength; i++) {
+      let randomNum = Math.floor(Math.random() * removedSymbols.length) + 1;
+      toGenerate.push(removedSymbols[randomNum]);
+    }
+    console.log(removedSymbols.length);
+    console.log("numbers checked");
+  } else if (withSymbols.checked === true) {
+    for (let i = 0; i < pwLength; i++) {
+      let randomNum = Math.floor(Math.random() * removedNumbers.length) + 1;
+      toGenerate.push(removedNumbers[randomNum]);
+    }
+
+    console.log("symbols checked");
+  } else {
+    console.log("none selected");
+
+    for (let i = 0; i < pwLength; i++) {
+      let randomNum = Math.floor(Math.random() * noChecked.length) + 1;
+      toGenerate.push(noChecked[randomNum]);
+    }
+  }
+}
+
+console.log(passwordLength.value);
